ddb120c0fc737bcd4ab7079cfb2b98480125d4c1
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-08-21 22:35:45 +0600

    add more code in linked list

--- DS/Cpp/LinkedList.cpp
+++ DS/Cpp/LinkedList.cpp
@@ -32,6 +32,64 @@ void insertAtTail(Node* &head, int value) {
     temp->next = newNode;
 }
 
+void insterAtAnyPos(Node* &head, int pos, int value) {
+    Node *newNode = new Node(value);
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    if(pos == 1) {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+    Node *temp = head;
+    for(int i = 1; i < pos-1; i++) {
+        temp = temp->next;
+    }
+    newNode->next = temp->next;
+    temp->next = newNode;
+}
+
+void deleteFromHead(Node* &head) {
+    head = head->next;
+}
+
+void deleteFromTail(Node* &head) {
+    Node *temp1;
+    Node *temp = head;
+    while(temp->next != NULL) {
+        temp1 = temp;
+        temp = temp->next;
+    }
+    temp1->next = NULL;
+}
+
+void deleteFromAnyPos(Node* &head, int pos) {
+    Node *temp = head;
+    for (int i = 1; i < pos - 1; i++) {
+        temp = temp->next;
+    }
+    if(pos == 1) {
+        head = head->next;
+        return;
+    }
+    Node *deleteNode = temp->next;
+    temp->next = temp->next->next;
+    delete deleteNode;
+}
+
+bool search(Node* head, int key) {
+    Node *temp = head;
+    while(temp != NULL) {
+        if(temp->data == key) {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
 void display(Node* head) {
     Node *temp = head;
     while(temp != NULL) {
@@ -48,6 +106,17 @@ int main() {
     insertAtTail(head, 3);
     display(head);
     insertAtHead(head, 4);
+    display(head);
+    cout << search(head, 3) << endl;
+    insterAtAnyPos(head, 2, 5);
+    insterAtAnyPos(head, 3, 6);
+    insterAtAnyPos(head, 1, 10);
+    display(head);
+    deleteFromHead(head);
+    display(head);
+    deleteFromTail(head);
+    display(head);
+    deleteFromAnyPos(head, 1);
     display(head);
     return 0;
 }
