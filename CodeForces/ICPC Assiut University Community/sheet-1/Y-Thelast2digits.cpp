b098cbcad42ce441fa2fed21c546a71d02c6ac95
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-12-17 14:06:33 +0600

    all changes

--- CodeForces/ICPC Assiut University Community/sheet-1/Y-Thelast2digits.cpp
+++ CodeForces/ICPC Assiut University Community/sheet-1/Y-Thelast2digits.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define BOOST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define BOOST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 
 int main() {
     BOOST;
