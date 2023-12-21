97f44e295e98750a3323a0fdf068417fbe0334d9
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-12-22 00:21:47 +0600

    file name change

---
+++ CodeForces/ICPC Assiut University Community/sheet-2 (Loops)/A-1toN.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define BOOST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+#define LL long long
+#define FOR(i, a, b) for(int i = a; i <= b; ++i)
+#define RFOR(i, a, b) for(int i = a; i >= b; --i)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PRINT(x) cout << x << endl;
+#define FMT(x) fixed << setprecision(x)
+
+int main() {
+    BOOST;
+    int n;
+    cin >> n;
+    FOR(i, 1, n)
+        PRINT(i);
+    return 0;
+}
---
+++ CodeForces/ICPC Assiut University Community/sheet-2 (Loops)/B-EvenNumbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define BOOST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+#define LL long long
+#define FOR(i, a, b) for(int i = a; i <= b; ++i)
+#define RFOR(i, a, b) for(int i = a; i >= b; --i)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PRINT(x) cout << x << endl;
+#define FMT(x) fixed << setprecision(x)
+
+int main() {
+    BOOST;
+    LL n;
+    cin >> n;
+    FOR(i, 2, n) {
+        if(i % 2 == 0) {
+            PRINT(i);
+        }
+    }
+    return 0;
+}
