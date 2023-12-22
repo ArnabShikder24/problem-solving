d6f45f43d48f20d0757b0db1075df4851e62bf86
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-12-22 22:56:17 +0600

    2 problem solved

---
+++ CodeForces/ICPC Assiut University Community/sheet-2 (Loops)/H-OnePrime.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define BOOST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+#define LL long long
+#define FOR(i, a, b) for(int i = a; i <= b; ++i)
+#define RFOR(i, a, b) for(int i = a; i >= b; --i)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PRINT(x) cout << x << endl;
+#define FMT(x) fixed << setprecision(x)
+
+bool isPrime(int num) {
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main() {
+    BOOST;
+    int n;
+    cin >> n;
+    if (isPrime(n))
+    {
+        YES;
+    }else {
+        NO;
+    }
+    
+    return 0;
+}
