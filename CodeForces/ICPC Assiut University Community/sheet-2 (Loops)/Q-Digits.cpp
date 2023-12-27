4d6ab4bc50fd514c9fa82ee611df967ba54d4efd
ArnabShikder24 <arnabshikder2001@gmail.com>
2023-12-28 03:04:23 +0600

    3 problem added

---
+++ CodeForces/ICPC Assiut University Community/sheet-2 (Loops)/Q-Digits.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define BOOST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define endl '\n'
+#define LL long long
+#define FOR(i, a, b) for(int i = a; i <= b; ++i)
+#define RFOR(i, a, b) for(int i = a; i >= b; --i)
+#define YES cout << "YES" << endl;
+#define NO cout << "NO" << endl;
+#define DEBUG(x) cout << #x << " = " << x << endl;
+#define PRINT(x) cout << x << endl;
+#define FMT(x) fixed << setprecision(x)
+
+int main() {
+    BOOST;
+    int n;
+    cin >> n;
+    while(n--) {
+        string s;
+        cin >> s;
+        reverse(s.begin(), s.end());
+        for(char i: s)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
