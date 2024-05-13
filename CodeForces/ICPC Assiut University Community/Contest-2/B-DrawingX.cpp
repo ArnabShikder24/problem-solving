f06088b1665713dcf93c71e348a9b695a0d0f0cd
ArnabShikder24 <arnabshikder2001@gmail.com>
2024-05-14 03:43:12 +0600

    drawing x problem solved

--- CodeForces/ICPC Assiut University Community/Contest-2/B-DrawingX.cpp
+++ CodeForces/ICPC Assiut University Community/Contest-2/B-DrawingX.cpp
@@ -15,11 +15,23 @@ int main() {
   BOOST;
   int n;
   cin >> n;
-  for (int i = 1; i < n; i++) {
-    for (int i = 1; i < n; i++) {
-      if(n == n) {
-        cout << "\\ ";
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if(i == j) {
+        if(i == n / 2 && j == n / 2) {
+          cout << "X";
+        }
+        else {
+          cout << "\\";
+        }
       }
+      else if(i + j == n - 1) {
+        cout << "/";
+      }
+      else {
+        cout << "*";
+      }
+
     }
     cout << endl;
   }
